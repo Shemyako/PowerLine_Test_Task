47aa9d14f8ceaa67d2fd5d27858d02b1c0d45784
Shemyako <52855609+Shemyako@users.noreply.github.com>
2022-08-10 14:05:22 +0300

    SQL task done

---
+++ Second_task.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [Customers] (
+	Id int PRIMARY KEY IDENTITY,
+	Name varchar(255) NOT NULL
+)
+GO
+CREATE TABLE [Orders] (
+	Id int PRIMARY KEY IDENTITY,
+	CustomerId int REFERENCES [Customers] (Id) ON DELETE CASCADE
+
+)
+
+GO
+INSERT INTO [Customers] VALUES ('Max'), ('Pavel'), ('Ivan'), ('Leonid')
+GO
+INSERT INTO [Orders] VALUES (2),(4)
+GO
+
+SELECT Name FROM [Customers] LEFT JOIN [Orders] ON CustomerId = [Customers].Id WHERE CustomerId IS NULL
